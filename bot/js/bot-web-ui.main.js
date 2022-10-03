ec72cff7e58d3784c450430fb27c39032fe1cbaf
Thisyahlen <thisyahlen@regentmarkets.com>
2022-10-03 11:21:53 +0800

    Updates

--- bot/js/bot-web-ui.main.js
+++ bot/js/bot-web-ui.main.js
@@ -232,7 +232,7 @@ module.exports = __WEBPACK_EXTERNAL_MODULE_react_dom__;
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = (chunkId) => {
 /******/ 			// return url for filenames based on template
-/******/ 			return "bot/js/bot." + chunkId + "." + {"vendors-node_modules_deriv_deriv-api_dist_DerivAPIBasic_js-node_modules_deriv_js-interpreter_-7c5351":"1ac3818642269278d368","bot-web-ui-app":"b929ddb26a972d703400","dalembert-xml":"ffd5ebe7f62d0ca4fb0a","martingale-xml":"9c5372ab9faaca4ce96a","oscars_grind-xml":"23b9b94bb7a3945734c8","dbot-collection":"a93810fed0b113f239de"}[chunkId] + ".js";
+/******/ 			return "bot/js/bot." + chunkId + "." + {"vendors-node_modules_deriv_deriv-api_dist_DerivAPIBasic_js-node_modules_deriv_js-interpreter_-7c5351":"1ac3818642269278d368","bot-web-ui-app":"184f82a95c64f3b8526d","dalembert-xml":"ffd5ebe7f62d0ca4fb0a","martingale-xml":"9c5372ab9faaca4ce96a","oscars_grind-xml":"23b9b94bb7a3945734c8","dbot-collection":"a93810fed0b113f239de"}[chunkId] + ".js";
 /******/ 		};
 /******/ 	})();
 /******/ 	
