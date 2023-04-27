71e752087a9dcc4fe3ea59348d52b1fe8d24a930
Thisyahlen Nair <thisyahlen@regentmarkets.com>
2023-04-27 13:42:24 +0800

    fix: trade table error

--- packages/reports/src/Helpers/profit-loss.ts
+++ packages/reports/src/Helpers/profit-loss.ts
@@ -1,7 +1,1 @@
-export const getProfitOrLoss = (value: string) => {
-    if (!value) {
-        return 'unknown';
-    }
-    const amount = value.replace(/,/g, '');
-    return +amount >= 0 ? 'profit' : 'loss';
-};
+export const getProfitOrLoss = (value: string) => (+value.replace(/,/g, '') >= 0 ? 'profit' : 'loss');
